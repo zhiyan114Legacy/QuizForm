6a89caec8e21b9c12adf52e7a1588dbc0a5ba078
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-04-22 11:54:25 -0400

    Added form validation and small fixes

--- Main.js
+++ Main.js
@@ -1,3 +1,60 @@
     
       particlesJS.load('particle', 'particle.conf.json', function() {})
+      var MissingAnswer = "Please pick an answer."
       $('.ui.radio.checkbox').checkbox();
+      $("#mainform").form({
+          Name: {
+              identifier:"Name",
+              rules: [
+                  {
+                    type: 'empty',
+                    prompt: "You need to put your name or you'll get a 0% LOL",
+                  }
+              ]
+          },
+          q1:{
+              identifier: "q1",
+              rules:[
+                  {
+                    type: 'checked',
+                    prompt: MissingAnswer,
+                  }
+              ]
+          },
+          q2:{
+            identifier: "q2",
+            rules:[
+                {
+                  type: 'checked',
+                  prompt: MissingAnswer,
+                }
+            ]
+          },
+          q3:{
+            identifier: "q3",
+            rules:[
+                {
+                  type: 'checked',
+                  prompt: MissingAnswer,
+                }
+            ]
+        },
+        q4:{
+            identifier: "q4",
+            rules:[
+                {
+                  type: 'checked',
+                  prompt: MissingAnswer,
+                }
+            ]
+        },
+        q5:{
+            identifier: "q5",
+            rules:[
+                {
+                  type: 'checked',
+                  prompt: MissingAnswer,
+                }
+            ]
+        },
+      })
