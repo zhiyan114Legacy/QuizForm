29cf5f7d3bb8e535d2351eb9c4fde7841a7e2cb8
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2021-04-22 11:26:51 -0400

    Completed Form Design

--- Main.js
+++ Main.js
@@ -1,3 +1,3 @@
-      // In-line script, not a very safe way to run js but oh well.
-      particlesJS.load('particle', 'particle.conf.json', function() { // Do nothing
-      })
+    
+      particlesJS.load('particle', 'particle.conf.json', function() {})
+      $('.ui.radio.checkbox').checkbox();
